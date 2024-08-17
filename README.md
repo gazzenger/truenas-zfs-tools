70b60993ab35153940001fc8547202fa1c350f4b
Gary Namestnik <gazzenger@gmail.com>
2024-08-17 14:27:16 +0800

    Init Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## ZFS-Unlock-SSH
+
+This tool is adapted to allow unlocking a Truenas ZFS dataset encrypted by a passphrase.
+
+Ensure the user has already been configured for SSH access via public/private key pairs with the Truenas Instance.
+
+This script has been adapted from the following sources,
+* https://matouschek.org/blog/truenas-scale-unlock-from-cli/
+* https://gist.github.com/mcdamo/ed7cdcaf51069cddd025b4bc1adf2250
+
+The main purpose of this method is my preference to utilise CLI tooling rather than needing to perform HTTP requests to get the job done.
+
+This tool takes an optional argument if you want to lock the datasets, i.e. ./zfs-unlock-ssh.sh lock
+
+You must provide a .env file containing the following
+#SSH_HOST=
+#SSH_USER=
+#datasets=()
+
+When the command is run the datasets are looped over, and the passphrase is used for unlocking or locking the dataset.
+
+
---
+++ zfs-unlock-ssh.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+## Script to unlock TrueNAS dataset(s) by way of typed passphrase
+
+set -e
+
+# get dir of this script to load conf
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. $DIR/.env
+
+# the following should be defined in conf file:
+#SSH_HOST=
+#SSH_USER=
+#datasets=()
+
+#check for input arg $1
+# $1: lock or unlock, unlock is the default
+ACTION=${1:-unlock}
+
+for dataset in "${datasets[@]}"; do
+  if [ "$ACTION" == "lock" ]; then
+    echo -n "Locking $dataset..."
+    ssh $SSH_USER@$SSH_HOST "midclt call pool.dataset.lock \"${dataset}\" &> /dev/null"
+  else
+    read -s -p "Enter passphrase for dataset $dataset: " PASS
+    echo
+    echo -n "Unlocking $dataset..."
+    ssh $SSH_USER@$SSH_HOST "midclt call pool.dataset.unlock \"${dataset}\" \"{\\\"datasets\\\": [{\\\"name\\\": \\\"${dataset}\\\",\\\"passphrase\\\": \\\"${PASS}\\\"}]}\" &> /dev/null"
+  fi
+
+# Sleep 5s before checking if it is unlocked
+  sleep 5s
+  RESULT=$(ssh $SSH_USER@$SSH_HOST "midclt call pool.dataset.query \"[[\\\"id\\\", \\\"=\\\", \\\"${dataset}\\\"]]\" | jq '.[0].locked'")
+  if [ "$ACTION" == "lock" ]; then
+    if [[ "$RESULT" == "true" ]]; then echo "Success"; else echo "Failed"; fi
+  else
+    if [[ "$RESULT" == "true" ]]; then echo "Failed"; else echo "Success"; fi 
+  fi
+
+  echo
+done
+
